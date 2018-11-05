69a93eed26547f4f0f8bf04c8acf52341687371a
Kaushik Mohan <kaushik.s.mohan@gmail.com>
2018-11-05 13:40:41 -0500

    view and install binder functions

--- R/BaseViewsFunctions.R
+++ R/BaseViewsFunctions.R
@@ -1,31 +1,47 @@
-## List of views- based on ctv available.views ## 
-
-#' List Views available
+#' List Binders
 #'
-#' This function allows you to see the list of views available.
-#' @param Nothin
-#' @keywords views
-#' list.base.views()
+#' Function to see the binders of packages available and a summary for each. 
+#' @usage list_binders()
+#' @param compartment The compartment to view the binders in. Defaults to "all". 
+#' - "all" to view all the binders
+#' - "master" to view binders that came with the package
+#' - "user" to view binders created by the user
+#' @examples list_binders(compartment="master")
+#' 
+#' list_binders()
 
-list.base.views <- function(){
+list_binders <- function(compartment="all"){
   load("./R/BaseViews.rda")
   x <-as.character(unique(Topics.Views$Topic))
+  ## To Do: Add number of packages column, concatenated head of packages
   rm(Topics.Views)
   return(x)
 }
 
-## Test ## 
 
-# list.base.views()
+#' Install Binders
+#'
+#' Function to install the packages in the binders. 
+#' @usage install_binders(binders)
+#' @param binders The binder to install 
+#' - "all" to view all the binders
+#' - "master" to view binders that came with the package
+#' - "user" to view binders created by the user
+#' @examples install_binders("MLM")
+#' 
+#' install_binders()
 
-## Installing views - based on ctv install.views ##  -
 
-install.base.views <- function(views){
+install_binders <- function(binders){
   load("./R/BaseViews.rda")
   Topics.Views$Topic <- as.character(Topics.Views$Topic)
   Topics.Views$Package <- as.character(Topics.Views$Package)
-  pkgs<-na.omit(ifelse(is.element(Topics.Views$Topic, views), Topics.Views$Package, NA))
+  
+  # pkgs<-na.omit(ifelse(is.element(Topics.Views$Topic, binders[i]), Topics.Views$Package, NA))  
+  
+  pkgs <- Topics.Views$Package[Topics.Views$Topic %in% binders]
+  pkgs <- paste(pkgs,collapse = ",")
   rm(Topics.Views)
-  for(i in seq_along(pkgs)) install.packages(pkgs[[i]])
+  install.packages(pkgs)
   invisible()
 }

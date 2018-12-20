bfc113f526914e5bf263993295026b9fd5a4cb5c
Madison Volpe <mv1617@nyu.edu>
2018-12-20 13:38:09 -0500

    Madison dataset documentation

---
+++ R/ExampleEFA-data.R
@@ -0,0 +1,28 @@
+#' Example EFA dataset
+#'
+#' Student responses on course evaluations. 
+#' Adapted from a dataset from UCLA's
+#' Institute for Digital Research and Education.
+#'
+#' @docType data
+#' @usage data(ExampleEFA)
+#' @format A dataframe of 1365 observations and 13 variables.
+#' \describe{
+#'   \item{X}{Unique ID}
+#'   \item{Item 13}{Instructor well prepared}
+#'   \item{Item 14}{Instructor scholarly grasp}
+#'   \item{Item 15}{Instructor confidence}
+#'   \item{Item 16}{Instructor focus lectures}
+#'   \item{Item 17}{Instructor uses clear relevant examples}
+#'   \item{Item 18}{Instructor sensitive to students}
+#'   \item{Item 19}{Instructor allows me to answer questions}
+#'   \item{Item 20}{Instructor accessible to students outside class}
+#'   \item{Item 21}{Instructor aware of students understanding}
+#'   \item{Item 22}{I am satisfied with student performance evaluation}
+#'   \item{Item 23}{Compared to other instructors, this instructor is}
+#'   \item{Item 23}{Compared to other courses this course was}
+#' }
+#' @keywords datasets
+#' @source \href{https://stats.idre.ucla.edu/spss/output/factor-analysis/}
+#' {Institute for Digital Research and Education}
+"ExampleEFA"
---
+++ R/anxiety-data.R
@@ -0,0 +1,18 @@
+#' Anxiety Dataset
+#'
+#' An individual's math and english scores based on their anxiety level. 
+#' Dataset is useful for running a One-Way Manova. Dataset is 
+#' simulated but draws inspiration from Laerd Statistics. 
+#'
+#' @docType data
+#' @usage data(anxiety)
+#' @format A dataframe of 1000 observations and 3 variables.
+#' \describe{
+#'   \item{math}{math scores}
+#'   \item{english}{english scores}
+#'   \item{anxiety}{anxiety level (low, med, high)}
+#' }
+#' @keywords datasets
+#' @source \href{https://statistics.laerd.com/stata-tutorials/one-way-manova-using-stata.php}
+#' {Laerd Statistics}
+"anxiety"
